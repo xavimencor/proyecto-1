6a99d4bac8c175fac283c4aff30765a165b264b6
xavimencor <mendoza.xavi@gmail.com>
2016-05-31 16:59:39 -0600

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -1,49 +1,53 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+$(document).ready(function(){
+    $(".num").click(function(){
+        var numero = $(this).val(); 
+        $("#edit1").val($("#edit1").val()+ numero);
+     });
+});
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+$("#limp").click(function(){
+    $("#edit1").val("");
+})
 
-        console.log('Received Event: ' + id);
-    }
-};
+$("#mm").click(function(){
+    m1 = parseFloat($("#edit1").val());
+    $("#edit1").val(m1 * -1);
+})
+
+$("#porce").click(function(){
+    p1 =parseFloat($("#edit1").val());
+    $("#edit1").val(p1 / 100);
+})
+
+$(".op").click(function(){
+    n1=parseFloat($("#edit1").val());
+    $("#edit1").val("");
+    op1 = $(this).val();
+    
+});
+
+$("#np").click(function(){
+    var c = $("#edit1").val().indexOf(".");
+    if (c==-1){
+        $("#edit1").val($("#edit1").val()+".");
+    }    
+});
+
+$("#resul").click(function(){
+    if (op1=="/"){
+        var n2=parseFloat($("#edit1").val());
+        $("#edit1").val(n1/n2);
+    }else
+    if (op1=="*"){
+        var n2=parseFloat($("#edit1").val());
+        $("#edit1").val(n1*n2);
+    }else
+    if (op1=="+"){
+        var n2=parseFloat($("#edit1").val());
+        $("#edit1").val(n1+n2);
+    } else
+    if (op1=="-"){
+        var n2=parseFloat($("#edit1").val());
+        $("#edit1").val(n1-n2);
+    }    
+});
